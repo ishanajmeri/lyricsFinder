5bf4fa234e3728deda985babf4aef9951860ea46
ishanajmeri <ajmeriishansurat@gmail.com>
2020-09-10 16:04:18 +0530

    ex

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -29,11 +29,6 @@ const Home = () => {
         <h4 style={{ textAlign: 'center', color: '#FF1493' }}>
           “I’ve had great success being a total idiot.” – Jerry Lewis
         </h4>
-        {/* <Card bordered={false} style={{ backgroundColor: '#282828' }}>
-          <Slider {...settings}>
-           <Artists />
-          </Slider>
-        </Card> */}
         <Card>
           <img
             alt="example"
@@ -41,6 +36,11 @@ const Home = () => {
             style={{ width: 150, height: 150, borderRadius: '50%' }}
           />
         </Card>
+        {/* <Card bordered={false} style={{ backgroundColor: '#282828' }}>
+          <Slider {...settings}>
+           <Artists />
+          </Slider>
+        </Card> */}
       </Card>
     </div>
   );
